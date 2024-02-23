8867224e5d83fa052a36558a3c18cd834ac849c6
SuneelKum12345 <snkmr231@gmail.com>
2024-02-23 09:47:48 +0530

    Cgtc ver uploading on githubsomethin

---
+++ Datatype.js
@@ -0,0 +1,23 @@
+let suneel = "One Person";
+suneel = "Person"
+
+const Id = 124235;
+
+console.log(suneel);
+console.log(Id);
+
+
+var geek = "Hello Geek"
+let $ = "Welcome"
+const _example = "Gfg"
+
+var a = "Hello Geeks"
+var b = 10;
+var c = 12;
+var d = b+c;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
---
+++ newvariables.js
@@ -0,0 +1,162 @@
+let x= 10
+let y=20
+
+let z= x+y;
+let m= x-y;
+let v= x*y;
+let n= x/y;
+
+console.log("sun " + z);
+console.log("sun " + m);
+console.log("sun " + v);
+console.log("sun " + n);
+
+let sum;
+sum = 605;
+
+const sum1 = 35
+
+let sritng = " what 50 so ever written in this double inverted comma is string"
+
+console.log(sritng);
+
+let numb = 20;
+console.log(numb);
+
+let boolen = true;
+console.log(boolen);
+console.log(typeof boolen);
+
+let someU = undefined
+console.log(typeof someU);
+
+let someNl = null;
+console.log(someNl)
+console.log(typeof someNl)
+
+let  object = "Johan"
+console.log(object);
+const person = { first : "Mohan", last : "Doe"}
+let first 
+console.log(first)
+
+let firstsym = Symbol(50);
+
+let secondsym = Symbol("second symbol");
+console.log(firstsym, secondsym);
+console.log(typeof firstsym);
+
+let thirdsym = Symbol(60);
+let fouthsym = Symbol(60);
+console.log(firstsym, thirdsym);
+console.log(typeof thirdsym);
+console.log(thirdsym);
+console.log(secondsym, thirdsym);
+console.log(thirdsym == fouthsym);
+console.log(firstsym == thirdsym);
+
+
+let bigBin = BigInt("0b1010101001010101001111111111111111");
+console.log(bigBin);
+let sumBig = BigInt(5566664566446445645688945489);
+console.log(sumBig);
+console.log(typeof sumBig);
+
+let SumInt = 5645645645645645645645456n;
+console.log(SumInt);
+console.log(typeof SumInt);
+
+
+let symbol1 = Symbol("Geeks")
+let symbol2 = Symbol("Geeks")
+console.log(symbol1 == symbol2);
+
+let second = new Object(20);
+console.log(second);
+
+var a = 17; 
+var b = "GeeksforGeeks"; 
+var c = ""; 
+var d = null; 
+var f = 15.2;
+  
+console.log("Type of a = " + (typeof a)); 
+console.log("Type of b = " + (typeof b)); 
+console.log("Type of c = " + (typeof c)); 
+console.log("Type of d = " + (typeof d)); 
+console.log("Type of e = " + (typeof e));
+console.log("Type of f = " + (typeof f));
+
+const car = {
+    model: "Modelname",
+    name: "Mg",
+    no: 404
+}
+console.log(car);
+
+var geek = "Hello Geek"
+let $ = "Welcome"
+const _example = "Gfg"
+
+var a = "Hello Geeks"
+var b = 10;
+var c = 12;
+var d = b+c;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+let w= "Hello learners"
+let s = "joinig";
+let k = "12";
+let g = b+c;
+
+src = "https://www.geeksforgeek.org/js/script.js"
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+const o = "Hello learners"
+console.log(a);
+ 
+const q = 400;
+console.log(b);
+ 
+const l = "12";
+console.log(l);
+
+console.log("Hello World!")
+let hero = 'Computer science portal';
+console.log(hero)
+
+let sub = 5-3
+console.log(sub)
+let mul = 7*2
+console.log(mul)
+let div = 8/9
+console.log(div)
+
+const fruit = "Apple"
+ 
+switch (fruit) {
+    case "Apple":
+        console.log("Apple is healthy for our body");
+        break;
+    case "Mango":
+        console.log("Mango is a National fruit of India");
+        break;
+    default:
+        console.log("I don't like fruits.");
+}
+
+
+
+
+
+
+
